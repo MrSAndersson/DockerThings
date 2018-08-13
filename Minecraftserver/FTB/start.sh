c0ce322092bcb3a6584ff5b75d60fc0f957f4a56
Stefan Andersson <stefan@bentusi.standersson.se>
2018-08-13 22:24:59 +0200

    Added Revamp and made updates better

--- Minecraftserver/FTB/start.sh
+++ Minecraftserver/FTB/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-mv /home/minecraft/zip/* /home/minecraft/server/
 cd /home/minecraft/server
+
+if [ -z "$(ls -A /home/minecraft/zip)" ]; then
+   echo "Resuming Using the same FTB version"
+else
+    echo "New container, override FTB files"
+    rm -rf config libraries modpack mods version.json FTBServer-*
+    mv /home/minecraft/zip/* .
+fi
+
+
+
 ./ServerStart.sh
