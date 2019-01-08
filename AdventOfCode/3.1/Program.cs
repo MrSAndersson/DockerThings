546ec53d33917685f7a7b90f3b9f8ed6e1d29812
MrSAndersson <stefan.andersson@nepa.com>
2019-01-08 08:49:48 +0100

    Added Claim container class and a file stream reader

--- AdventOfCode/3.1/Program.cs
+++ AdventOfCode/3.1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
 
 namespace _3._1
 {
+
+    class Claim
+    {
+        public int xOrigin { get; set; }
+        public int yOrigin { get; set; }
+        public int xLength { get; set; }
+        public int yLength { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Claim> claims = new List<Claim>();
+            FileStream filestream = new FileStream("input.txt", FileMode.Open);
+            using (StreamReader reader = new StreamReader(filestream))
+            {
+                string newClaim;
+                while ((newClaim = reader.ReadLine()) != null)
+                {
+
+                }
+            }
         }
     }
 }
