972e4e86da4fbff35b7dca4c47e2d848a9480f61
MrSAndersson <stefan.nigma@gmail.com>
2019-01-12 23:48:04 +0100

    Wrote comparison logic

--- AdventOfCode/3.1/Program.cs
+++ AdventOfCode/3.1/Program.cs
@@ -5,6 +5,25 @@ using System.IO;
 namespace _3._1
 {
 
+    class Coordinate : IEquatable<Coordinate>
+    {
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+
+        public bool Equals(Coordinate other)
+        {
+            return this.x == other.x &&
+                   this.y == other.y;
+        }
+    }
+
     class Claim
     {
         public int xOrigin { get; set; }
@@ -14,7 +33,7 @@ namespace _3._1
         public int xEnd { get; set; }
         public int yEnd { get; set; }
 
-        public void CheckOverlap(Claim otherClaim)
+        public List<Coordinate> CheckOverlap(Claim otherClaim, List<Coordinate> overlaps)
         {
             for (int x = xOrigin; x < xEnd; x++)
             {
@@ -23,10 +42,15 @@ namespace _3._1
                     if (otherClaim.xOrigin <= x && x <= otherClaim.xEnd &&
                         otherClaim.yOrigin <= y && y <= otherClaim.yEnd)
                     {
-                        //TODO have a list, check if the position is present and add if not
+                        Coordinate overlap = new Coordinate(x, y);
+                        if (!overlaps.Contains(overlap))
+                        {
+                            overlaps.Add(overlap);
+                        }
                     }
                 }
             }
+            return overlaps;
         }
     }
 
@@ -34,7 +58,9 @@ namespace _3._1
     {
         static void Main(string[] args)
         {
+            List<Coordinate> overlaps = new List<Coordinate>();
             List<Claim> claims = new List<Claim>();
+
             FileStream filestream = new FileStream("input.txt", FileMode.Open);
             using (StreamReader reader = new StreamReader(filestream))
             {
