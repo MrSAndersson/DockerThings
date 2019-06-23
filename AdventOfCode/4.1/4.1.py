b861e79b0dc37b5e998b2df783bb96e3d9212024
MrSAndersson <stefan.andersson@nepa.com>
2019-06-23 20:23:08 +0200

    Split off stuff into a module

--- AdventOfCode/4.1/4.1.py
+++ AdventOfCode/4.1/4.1.py
@@ -1,31 +1,5 @@
-class Guard:
-    def __init__(self):
-        self.sleep_times = []
-        self.total_sleep = 0
+import tools
 
 
-def import_data():
-    file = open("Input.txt")
-
-    data = []
-    for line in file:
-        data.append(line.rstrip('\n'))
-    data.sort()
-    return data
-
-
-data = import_data()
-
-guards = {}
-
-for line in data:
-    if "begins shift" in line:
-        guard_id = line.split('#', 1)[1].split(' ', 1)[0]
-        if not guards.__contains__(guard_id):
-            guards[guard_id] = Guard()
-    elif "falls asleep" in line:
-        start = int(line.split(':', 1)[1].split(']', 1)[0])
-    else:
-        end = int(line.split(':', 1)[1].split(']', 1)[0])
-        guards[guard_id].sleep_times.append('{} {}'.format(start, end))
-        guards[guard_id].total_sleep += end - start + 1
+guards = tools.prepare_data()
+print(guards)
---
+++ AdventOfCode/4.1/tools.py
@@ -0,0 +1,33 @@
+class Guard:
+    def __init__(self):
+        self.sleep_times = []
+        self.total_sleep = 0
+
+
+def import_data():
+    file = open("Input.txt")
+
+    data = []
+    for line in file:
+        data.append(line.rstrip('\n'))
+    data.sort()
+    return data
+
+
+def prepare_data():
+
+    data = import_data()
+    guards = {}
+
+    for line in data:
+        if "begins shift" in line:
+            guard_id = line.split('#', 1)[1].split(' ', 1)[0]
+            if not guards.__contains__(guard_id):
+                guards[guard_id] = Guard()
+        elif "falls asleep" in line:
+            start = int(line.split(':', 1)[1].split(']', 1)[0])
+        else:
+            end = int(line.split(':', 1)[1].split(']', 1)[0])
+            guards[guard_id].sleep_times.append('{} {}'.format(start, end))
+            guards[guard_id].total_sleep += end - start + 1
+    return guards
