6d2ee7d00df75580a8dfefe44760080a9f782fd8
MrSAndersson <stefan.andersson@nepa.com>
2019-06-24 10:57:54 +0200

    Finished 4.1

--- AdventOfCode/4.1/4.1.py
+++ AdventOfCode/4.1/4.1.py
@@ -2,4 +2,7 @@ import tools
 
 
 guards = tools.prepare_data()
-print(guards)
+sleeper = tools.get_sleepiest_guard(guards)
+sleepiest_minute = sleeper.sleepiest_minute()
+
+print(sleeper.id * sleepiest_minute['minute'])
