3f661132345428a76abe828d2f4d6c3239cf1dbf
MrSAndersson <stefan.andersson@nepa.com>
2019-06-26 08:12:25 +0200

    did small work

--- AdventOfCode/5.1/app.py
+++ AdventOfCode/5.1/app.py
@@ -3,12 +3,14 @@ def is_uppercase(character):
         return True
 
 
-data = open("Input.txt").read()
+new_data = open("Input.txt").read()
 not_found = True
 while not_found:
+    data, new_data = new_data, ""
     for position in range(0, data.__sizeof__()):
         if data[position] == data[position].upper():
             if data[position + 1] == data[position].lower():
                 print("hej")
         elif data[position + 1] == data[position].upper():
             print("hoj")
+        else:
