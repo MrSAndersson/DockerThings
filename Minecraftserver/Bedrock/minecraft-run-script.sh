0f2e2e9473dd053d6f5e6e7773887d7674795b56
Stefan Svärd <stefan.svard@sportamore.se>
2020-05-06 21:42:39 +0200

    Pull before restarting

--- Minecraftserver/Bedrock/minecraft-run-script.sh
+++ Minecraftserver/Bedrock/minecraft-run-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+docker pull zappka/minecraft-bedrock:latest
 docker stop bedrock
 docker rm bedrock
-docker pull zappka/minecraft-bedrock:latest
-docker run --restart always -d -v bedrock:/home/minecraft/worlds -p 19132:19132 -p 35109:35109 --name=bedrock zappka/minecraft-bedrock:latest
+docker run --restart always -d -v bedrock:/home/minecraft/worlds -p 19132:19132/udp --name=bedrock zappka/minecraft-bedrock:latest
