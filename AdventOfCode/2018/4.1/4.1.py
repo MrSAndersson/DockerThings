7790ae4e02283934b2308747be15eeee6d60faf5
Stefan Svärd <stefan@bentus.svard.me>
2020-12-13 09:10:34 +0100

    Moved aoc to their own repos

--- AdventOfCode/2018/4.1/4.1.py
+++
@@ -1,8 +0,0 @@
-import tools
-
-
-guards = tools.prepare_data()
-sleeper = tools.get_sleepiest_guard(guards)
-sleepiest_minute = sleeper.sleepiest_minute()
-
-print(sleeper.id * sleepiest_minute['minute'])
